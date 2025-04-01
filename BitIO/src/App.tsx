77397b61e780c568245633b934869a9fc5ca2e77
Djafny Abdad <bakso@Djafnys-MacBook-Air.local>
2025-04-01 11:41:46 +0200

    feat: cleanup code structure

--- BitIO/src/App.tsx
+++ BitIO/src/App.tsx
@@ -1,28 +1,12 @@
-import { useState } from 'react';
-import { DndContext } from '@dnd-kit/core';
-import { Draggable, Droppable } from './components'
+import { DragDrop } from './components'
 import './App.css'
 
 const App = () => {
-  const [parent, setParent] = useState(null);
-  const draggable = (
-    <Draggable id="draggable">
-      Go ahead, drag me.
-    </Draggable>
-  );
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {!parent ? draggable : null}
-      <Droppable id="droppable">
-        {parent === "droppable" ? draggable : 'Drop here'}
-      </Droppable>
-    </DndContext>
+    <>
+      <DragDrop />
+    </>
   )
-
-  function handleDragEnd({ over }: any) {
-    setParent(over ? over.id : null);
-  }
-
 }
 
 export { App }
---
+++ BitIO/src/components/DragDrop/DragDrop.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { DndContext } from '@dnd-kit/core';
+import { Draggable, Droppable } from '../../components'
+
+const DragDrop = () => {
+    const [parent, setParent] = useState(null);
+    const draggable = (
+        <Draggable id="draggable">
+            Go ahead, drag me.
+        </Draggable>
+    );
+    return (
+        <DndContext onDragEnd={handleDragEnd}>
+            {!parent ? draggable : null}
+            <Droppable id="droppable">
+                {parent === "droppable" ? draggable : 'Drop here'}
+            </Droppable>
+        </DndContext>
+    )
+
+    function handleDragEnd({ over }: any) {
+        setParent(over ? over.id : null);
+    }
+
+}
+
+export { DragDrop }
